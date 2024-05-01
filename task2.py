781036dae5126e0d8b043023abed83d30c91a211
danilo4ka0 <149328398+danilo4ka0@users.noreply.github.com>
2024-05-01 18:06:54 +0200

    task number2

---
+++ task2.py
@@ -0,0 +1,35 @@
+def get_cats_info(path):
+    try:
+        # Відкриваємо файл і читаємо дані
+        with open(path, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+
+        # Ініціалізуємо список для зберігання інформації про котів
+        cats_info = []
+
+        # Обробляємо кожен рядок в файлі
+        for line in lines:
+            # Розділяємо рядок за комами
+            cat_data = line.strip().split(',')
+
+            # Перевіряємо, чи отримано правильні дані
+            if len(cat_data) == 3:
+                # Створюємо словник для кожного кота і додаємо його до списку
+                cat_info = {"id": cat_data[0], "cat name": cat_data[1], "age": cat_data[2]}
+                cats_info.append(cat_info)
+
+        return cats_info
+
+    except FileNotFoundError:
+        print(f"Файл '{path}' не знайдено.")
+        return None
+    except Exception as e:
+        print(f"Помилка при обробці файлу: {e}")
+        return None
+
+# Приклад використання функції
+cats_info = get_cats_info(r"C:\\Users\\demo\\Desktop\\my_r\\file.txt_2.txt")
+
+if cats_info is not None:
+    for cat in cats_info:
+        print(cat)
